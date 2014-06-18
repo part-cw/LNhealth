2b4204dd335b32bb2c3c654b62a1b1faf8808cfa
Matthias Görges <mgorges@cw.bc.ca>
2014-06-18 16:26:31 -0700

    IVUEPARSER: Add alarm parsing

--- constants.scm
+++ constants.scm
@@ -28,6 +28,10 @@
 (define NOM_ATTR_ID_BED_LABEL 		#x091E)
 (define NOM_ATTR_POLL_RTSA_PRIO_LIST 62010)
 (define NOM_ATTR_ALL_GROUPS 0)
+(define NOM_SAT_O2_TONE_FREQ      #xF008)
+(define NOM_ATTR_DEV_AL_COND      #x0916)
+(define NOM_ATTR_AL_MON_P_AL_LIST #x0902)
+(define NOM_ATTR_AL_MON_T_AL_LIST #x0904)
 
 ;; poll object types
 (define NOM_MOC_VMO_METRIC_NU 6)
@@ -41,5 +45,16 @@
 (define NOM_ACT_POLL_MDIB_DATA_EXT 61755)
 (define NOM_ATTR_POLL_OBJ_PRIO_NUM #xF228)
 (define NOM_PART_OBJ 1)
+
+;; alarm related
+(define NO_ALERT 0)
+(define LOW_PRI_T_AL 1)
+(define MED_PRI_T_AL 2)
+(define HI_PRI_T_AL 4)
+(define LOW_PRI_P_AL 256)
+(define MED_PRI_P_AL 512)
+(define HI_PRI_P_AL 1024)
+(define GEN_ALMON_INFO 513)
+(define STR_ALMON_INFO 516)
 
 ;; eof
