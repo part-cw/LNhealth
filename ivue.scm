c5d29a29421bad647c4ba8b3f5e73c407899e602
Matthias Görges <mgorges@cw.bc.ca>
2016-11-24 21:49:31 -0800

    IVUEPARSER: Change serial communication to follow S5 more closely

--- ivue.scm
+++ ivue.scm
@@ -4,17 +4,16 @@
 (include "ivue-encoders.scm")
 (include "ivue-request.scm")
 
+(define ivue:debuglevel 1)
+(define (ivue:log level . x) (if (>= ivue:debuglevel level) (apply log-system (append (list "ivue: ") x))))
 (define ivue:data #f)
 (define ivue:lastchecked 0.)
 (define ivue:attempt 0)
 
 (define (ivue:send dev msg debug)
-  (if debug (begin
-    (display "ivue S: ")
-    (for-each (lambda (cc) (begin (display (number->string cc 16)) (display " ")))
-      (u8vector->list msg))
-    (newline)
-  ))
+  (if debug
+    (ivue:log 0 "S: " (map (lambda (l) (number->string l 16)) (u8vector->list msg)))
+  )
   (let loop ((m (u8vector->list msg))(err #f))
     (if err
       (set! ivue:error #t)
@@ -28,17 +27,16 @@
 
 (define (ivue:recv dev debug)
   (set! ivue:data #f)
+  (set! ivue:error #t)
   (let* ((cache (serial-cache-read dev))
          (data (if cache (list->u8vector (map char->integer (string->list cache))) #f))
          (parseddata (if data (ivueparser:parseframe (u8vector->u8data data)) #f)))
     (if (u8data:sane? parseddata) (begin
       (set! ivue:data parseddata)
-      (if debug (begin
-        (display "ivue R: ")
-        (for-each (lambda (cc) (begin (display (number->string cc 16)) (display " ")))
-          (u8vector->list (u8data->u8vector ivue:data)))
-        (newline)
-      ))
+      (set! ivue:error #f)
+      (if debug
+        (ivue:log 0 "R: " (map (lambda (l) (number->string l 16)) (u8vector->list (u8data->u8vector ivue:data))))
+      )
     ))
   )
 )
@@ -58,7 +56,7 @@
           ;; If serial-try was successful return new runlevel 1
           (if newdev
             (begin
-              (if debug (display (string-append "ivue connected to " devname "\n")))
+              (if debug (ivue:log 0 (string-append "connected to " devname)))
               (instance-setvar! store instance "Device" newdev)
               (ivue:send newdev (ivue:arequest) debug)
               (serial-cache-setup newdev #xC0 #xC1) ;; Philips serial is framed between 0xc0 and 0xc1
@@ -72,14 +70,18 @@
       ((fx= runlevel 1)
         ;; Test if Association Response was positive or not
         (ivue:recv dev debug)
-        (if (and (u8data:sane? ivue:data) (fx= (u8data-ref ivue:data 0) #x0E))
+        (if (and ivue:data (fx= (u8data-ref ivue:data 0) #x0E))
           (begin
-            (if debug (display (string-append "ivue connection accepted \n")))
+            (ivue:log 1 "connect")
+            (store-event-add store 0 "Connect" instance)
+            (store-set! store "MonitorAlarm" '(0 "Connect" "Monitor"))
             (set! ivue:start ##now)
             2
           )
           (begin
-            (if debug (display (string-append "ivue connection refused \n")))
+            (ivue:log 1 "refused")
+            (store-event-add store 0 "Refused" instance)
+            (store-set! store "MonitorAlarm" '(0 "Refused" "Monitor"))
             (set! ivue:attempt (fx+ ivue:attempt 1))
             (if (fx= ivue:attempt 3)
               (begin
@@ -93,28 +95,28 @@
       )
       ((fx= runlevel 2)
         (ivue:recv dev debug)
-        (if (and (u8data:sane? ivue:data)
+        (if (and ivue:data
              (fx= (u8data-ref ivue:data 0) #xE1)
              (fx= (u8data-ref ivue:data 5) ROIV_APDU)
              (fx= (u8data-ref ivue:data 9) CMD_CONFIRMED_EVENT_REPORT))
           (begin
-            (if debug (display (string-append "ivue MDS Create Event received \n")))
             (ivue:send dev (ivue:mresult ivue:data) debug)
+            (instance-setvar! store instance "LastGood" ##now)
             3
           )
           (begin
-            (if (u8data:sane? ivue:data) (ivueparser store ivue:data))
+            (if ivue:data (ivueparser store ivue:data))
             2
           )
         )
       )
       ((fx= runlevel 3)
         (ivue:send dev (ivue:polltrends) debug)
-        (if debug (display (string-append "ivue Trends requested \n")))
+        (if debug (ivue:log 0 "Trends requested"))
         (if (instance-refvar store instance "Waveforms" #f)
           (begin
             (ivue:send dev (ivue:pollwaves) debug)
-            (if debug (display (string-append "ivue Waveforms requested \n")))
+            (if debug (ivue:log 0 "Waveforms requested"))
             (instance-setvar! store instance "Update" 0.05)
           )
           (instance-setvar! store instance "Update" 0.5)
@@ -130,53 +132,48 @@
   (let ((runlevel (instance-refvar store instance "RunLevel" 0))
         (dev (instance-refvar store instance "Device" #f))
         (debug (instance-refvar store instance "Debug" #f)))
-    (if (fx= runlevel 16)
-      (begin
+    (if (fx= runlevel 16) (begin
+      (let loop ()
         (ivue:recv dev debug)
-        ;; This gets new trends every 1 sec - also keeps waveform export alive.
-        (if (fl> (fl- ##now ivue:lastchecked) 1.) (begin
-          (if debug (begin
-            (display (store-listcat store "ivue"))
-            (newline)
-            (if (instance-refvar store instance "Waveforms" #f) (begin
-              (display (store-ref store "Pleth"))
-              (newline)
+        (if (or ivue:error (not ivue:data))
+          ;; we failed
+          (let ((lastgood (instance-refvar store instance "LastGood" 0.)))
+            (if (> (- ##now lastgood) 15) (begin
+              ;; prevent audible alarms outside of case
+              (ivue:log 1 "disconnect")
+              (store-event-add store (if (store-ref store "CaseID" #f) 4 0) "Disconnect" instance)
+              (store-set! store "MonitorAlarm" (list (if (store-ref store "CaseID" #f) 4 0) "Disconnect" "Monitor"))
+              ;; Clear all old values
+              (store-clear! store (map car (store-listcat store "ivue")))
+              (store-clear! store (map car (store-listcat store "ivue_display")))
+              ;; See if we can get a new monitor connect?
+              (instance-setvar! store instance "RunLevel" 0)
             ))
-          ))
-          (ivue:send dev (ivue:polltrends) debug)
-          (ivue:send dev (ivue:pollalarms) debug)
-          (set! ivue:lastchecked ##now)
-        ))
-        (if ivue:data
-          (begin
-            (if (u8data:sane? ivue:data)
-              (if (fx= (u8data-ref ivue:data 0) #x19)
-                (begin
-                  (log-error "ivue died after " (fl- ##now ivue:start) " sec")
-                  (display "IVUE DIED after ")(display (fl- ##now ivue:start))(display "sec")(newline)
-                  (instance-setvar! store instance "RunLevel" 0)
-                )
-                (ivueparser store ivue:data)
-              )
-            )
-            (instance-setvar! store instance "BadQueries" 0)
-
           )
-          (let ((missed (instance-refvar store instance "BadQueries" 0)))
-            (if (fx> missed (if (instance-refvar store instance "Waveforms" #f) 200 20))
-              (begin
-                ;; Clear all the stores
-                (store-clear! store (map car (store-listcat store "ivue")))
-                ;; Go back to search for monitors
+          ;; we are good
+          (begin
+            (instance-setvar! store instance "LastGood" ##now)
+            (if (fx= (u8data-ref ivue:data 0) #x19)
+              (let ((err (string-append "Connection died after " (number->string (fl- ##now ivue:start)) " sec")))
+                (ivue:log 1 err)
+                (store-event-add store (if (store-ref store "CaseID" #f) 4 0) err instance)
+                (store-set! store "MonitorAlarm" (list (if (store-ref store "CaseID" #f) 4 0) err "Monitor"))
                 (instance-setvar! store instance "RunLevel" 0)
-                (instance-setvar! store instance "BadQueries" 0)
               )
-              (instance-setvar! store instance "BadQueries" (fx+ missed 1))
+              (ivueparser store ivue:data)
             )
+            ;; This gets new trends every 1 sec - also keeps waveform export alive.
+            (if (fl> (fl- ##now ivue:lastchecked) 1.) (begin
+              (if debug (ivue:log 1 (store-listcat store "ivue")))
+              (ivue:send dev (ivue:polltrends) debug)
+              (ivue:send dev (ivue:pollalarms) debug)
+              (set! ivue:lastchecked ##now)
+            ))
+            (loop)
           )
         )
       )
-    )
+    ))
   )
 )
 
