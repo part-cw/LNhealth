b84b3fab730975475b36ebb7427194b132cbd779
Peter Lewis <peter.lewis@bcchr.ca>
2022-03-30 13:41:28 -0700

    PARA: v2.8
    Fixed security issue with custom loader, updated main.sx to current version from dropbox (removes videos, changes to score comparisons)

--- main.scm
+++ main.scm
@@ -1,6 +1,13 @@
 (main
 ;; initialization
-  (lambda (w h) (lnhealth-init 480 800))
+  (lambda (w h) (lnhealth-init 480 800 (lambda ()
+    (lnhealth:loadscript)
+    (let ((mainfile (string-append
+      (system-directory) (system-pathseparator)
+      "sandbox" (system-pathseparator)
+      "main.sx")))
+     (if (file-exists? mainfile)
+         (delete-file mainfile))))))
 ;; events
   (lambda (t x y) (lnhealth-events t x y))
 ;; termination
